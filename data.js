9b8e371782493c8db511bc98de37bc32149e8760
Sujay V <sujaywebdev@gmail.com>
2020-11-14 15:23:13 +0530

    Back end NODE.JS Code which is deployed in heroku
    Back end NODE.JS Code which is deployed in heroku

---
+++ data.js
@@ -0,0 +1,58 @@
+const data = [
+  {
+    url: "/videos/1c.mp4",
+    channel: "ZachKing",
+    description: "CakeWalk",
+    song: "Original MUSIC ....!!!",
+    likes: "123",
+    messages: "321",
+    shares: "999",
+  },
+  {
+    url: "/videos/2c.mp4",
+    channel: "ZachKing",
+    description: "Harry Potter",
+    song: "Original MUSIC ....!!!",
+    likes: "220",
+    messages: "451",
+    shares: "852",
+  },
+  {
+    url: "/videos/3c.mp4",
+    channel: "ZachKing",
+    description: "Wet Paint Inception",
+    song: "Original MUSIC ....!!!",
+    likes: "520",
+    messages: "258",
+    shares: "963",
+  },
+  {
+    url: "/videos/4c.mp4",
+    channel: "ZachKing",
+    description: "Bottle Cap Challenge",
+    song: "Original MUSIC ....!!!",
+    likes: "159",
+    messages: "753",
+    shares: "369",
+  },
+  {
+    url: "/videos/5c.mp4",
+    channel: "ZachKing",
+    description: "Hide and Seek",
+    song: "Original MUSIC ....!!!",
+    likes: "951",
+    messages: "324",
+    shares: "215",
+  },
+  {
+    url: "/videos/6c.mp4",
+    channel: "ZachKing",
+    description: "Museum Paint Come Alive",
+    song: "Original MUSIC ....!!!",
+    likes: "338",
+    messages: "601",
+    shares: "156",
+  },
+];
+
+export default data;
